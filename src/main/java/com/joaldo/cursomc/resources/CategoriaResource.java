fb50aa9fabb588b051cabef8e33d1c54e7f3827c
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-08-16 19:05:52 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/joaldo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joaldo/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
  package com.joaldo.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaldo.cursomc.domain.Categoria;
+import com.joaldo.cursomc.dto.CategoriaDTO;
 import com.joaldo.cursomc.services.CategoriaService;
 
 @RestController
@@ -32,14 +35,18 @@ public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
 		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
 		obj.setId(id);
+		
 		obj = service.update(obj);
+		
 		return ResponseEntity.noContent().build();
 		
 	}
@@ -48,5 +55,15 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
