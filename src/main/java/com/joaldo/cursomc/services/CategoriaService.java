7502b486d23e20e87f09a5fa7136acad955e004e
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-08-16 14:28:15 -0300

    DELETE de categoria implementado

--- src/main/java/com/joaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaldo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.joaldo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.joaldo.cursomc.domain.Categoria;
 import com.joaldo.cursomc.repositories.CategoriaRepository;
+import com.joaldo.cursomc.services.exceptions.DataIntegrityException;
 import com.joaldo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,5 +34,12 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
-	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
 }
