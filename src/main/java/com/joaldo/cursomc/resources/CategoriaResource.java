a1d5f612079f02dd47fb6b744a69cd7f7458c193
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-08-30 11:09:39 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/joaldo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joaldo/cursomc/resources/CategoriaResource.java
@@ -50,6 +50,7 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
 		Categoria obj = service.fromDTO(objDto);
+		
 		obj.setId(id);
 		
 		obj = service.update(obj);
